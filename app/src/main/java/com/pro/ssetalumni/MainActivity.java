2bbbdc6cd83020a633f8652c7c8762233e515316
ramdevcm <ramdevcm@gmail.com>
2020-10-02 11:09:44 +0530

    firebase connected - by Ramdev

--- app/src/main/java/com/pro/ssetalumni/MainActivity.java
+++ app/src/main/java/com/pro/ssetalumni/MainActivity.java
@@ -1,48 +1,50 @@
 package com.pro.ssetalumni;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
-    private Button buttonlogin;
-    private EditText eTLEmailAddress,eTLPassword;
-    private TextView registration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        eTLEmailAddress = (EditText)findViewById(R.id.eTLtEmailAddress);
-        eTLPassword = (EditText)findViewById((R.id.eTLPassword));
-        registration = (TextView)findViewById(R.id.textView2);
-        buttonlogin = (Button)findViewById(R.id.buttonlogin);
-
-    /*  buttonlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                validate();
-            }
-        }); */
-
-        registration.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SSET_reg.class));
-            }
-        });
-
+        Intent i = new Intent(MainActivity.this, Login.class);
+        startActivity(i);
     }
-  /*  private  void validate(String userName, String userPassword){
-        if((userName=="Admin") && (userPassword=="1234")){
-            Intent intent = new Intent(MainActivity.this, )
-            start
-        }*/
-
 }
--- app/src/main/java/com/pro/ssetalumni/SSET_reg.java
+++ app/src/main/java/com/pro/ssetalumni/SSET_reg.java
@@ -1,64 +1,165 @@
 package com.pro.ssetalumni;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class SSET_reg extends AppCompatActivity {
 
-    private EditText userName, userPassword, userEmail, userPhone;
-    private Button regButton;
-    private TextView userLogin;
+    EditText userName, userPassword, userEmail, userPhone;
+    Button regButton;
+    TextView userLogin;
+    public static final String TAG = "TAG";
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s_s_e_t_reg);
         setupUIViews();
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        //if already logged in then ...
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
 
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(validate() ){
-                    //Upload the data to the database
+                validate();
+                //Upload the data to the database
 
-                }
+
             }
         });
 
         userLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SSET_reg.this, MainActivity.class));
+                startActivity(new Intent(SSET_reg.this, Dashboard.class));
             }
         });
     }
-    private  void  setupUIViews(){
-        userName = (EditText)findViewById(R.id.eTPersonName);
-        userEmail = (EditText)findViewById(R.id.eTREmailAddress);
-        userPassword = (EditText)findViewById(R.id.eTRPassword);
-        userPhone = (EditText)findViewById(R.id.eTRPhone);
-        regButton = (Button)findViewById(R.id.buttonRegister);
-        userLogin =(TextView)findViewById(R.id.textView5);
+
+    private void setupUIViews() {
+        userName = (EditText) findViewById(R.id.eTPersonName);
+        userEmail = (EditText) findViewById(R.id.eTREmailAddress);
+        userPassword = (EditText) findViewById(R.id.eTRPassword);
+        userPhone = (EditText) findViewById(R.id.eTRPhone);
+        regButton = (Button) findViewById(R.id.buttonRegister);
+        userLogin = (TextView) findViewById(R.id.textView5);
     }
-    private Boolean validate(){
+
+    private void validate() {
         Boolean result = false;
 
-        String name = userName.getText().toString();
-        String password = userPassword.getText().toString();
-        String email = userEmail.getText().toString();
+        final String mName = userName.getText().toString();
+        String mPassword = userPassword.getText().toString();
+        final String mEmail = userEmail.getText().toString();
+        final String mPhone = userPhone.getText().toString();
+        if (TextUtils.isEmpty(mName)) {
 
-        if(name.isEmpty() || password.isEmpty() || email.isEmpty() ){
-            Toast.makeText(this,"Please enter all the details", Toast.LENGTH_SHORT);
-        }else{
-            result=true;
         }
-        return result;
+        if (TextUtils.isEmpty(mEmail)) {
+            userEmail.setError("Email is Required.");
+            return;
+        }
+
+        if (TextUtils.isEmpty(mPassword)) {
+            userPassword.setError("Password is Required.");
+            return;
+        }
+
+        if (mPassword.length() < 6) {
+            userPassword.setError("Password Must be >= 6 Characters");
+            return;
+        }
+        if (mPhone.length() < 10 && mPhone.length() > 11) {
+            userPhone.setError("Enter a valid 10 digit number");
+        }
+        progressBar.setVisibility(View.VISIBLE);
+
+        //register the user to firebase
+
+        fAuth.createUserWithEmailAndPassword(mEmail, mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    // send verification link
+
+                    FirebaseUser fuser = fAuth.getCurrentUser();
+                    fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(SSET_reg.this, "Verification Email Has been Sent.", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG,"onFailure: Email not sent " + e.getMessage());
+                        }
+                    });
+
+                    Toast.makeText(SSET_reg.this, "User Created.", Toast.LENGTH_SHORT).show();
+                    userID = fAuth.getCurrentUser().getUid();
+                    DocumentReference documentReference = fStore.collection("users").document(userID);
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("fName", mName);
+                    user.put("email", mEmail);
+                    user.put("phone", mPhone);
+                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "onSuccess: user Profile is created for " + userID);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "onFailure: " + e.toString());
+                        }
+                    });
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                } else {
+                    Toast.makeText(SSET_reg.this, "Error ! " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
     }
+
 }
