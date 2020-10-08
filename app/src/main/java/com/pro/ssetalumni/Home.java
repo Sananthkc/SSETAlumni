4d08d0921c58bc5d9842b221cb35f1bba1496ebb
Sananth k c <sananth1010@gmail.com>
2020-10-08 16:37:56 +0530

    home page

--- app/src/main/java/com/pro/ssetalumni/Home.java
+++ app/src/main/java/com/pro/ssetalumni/Home.java
@@ -1,14 +1,23 @@
 package com.pro.ssetalumni;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Home extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
     }
 }
