09b4bcc2a34046a9ee731a92c9205a846762d943
ramdevcm <ramdevcm@gmail.com>
2020-10-13 00:19:21 +0530

    fixed home array adapter bug- ramdev

--- app/src/main/java/com/pro/ssetalumni/EventPost.java
+++ app/src/main/java/com/pro/ssetalumni/EventPost.java
@@ -1,147 +1,63 @@
 package com.pro.ssetalumni;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.text.TextUtils;
+
+import android.util.Log;
 import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.EditText;
+
 import android.widget.Toast;
 
 
-import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class EventPost extends AppCompatActivity {
-
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolbar;
-    ActionBarDrawerToggle toggle;
-    private RecyclerView mRecyclerView;
-    private ImageAdapter mAdapter;
-    private ProgressBar mProgressCircle;
-    private DatabaseReference mDatabaseRef;
-    private List<UploadImage> mUploads;
-
-    String fullname;
-    TextView fullName;
-
-    BottomNavigationView bottomNavigationView;
-
+    EditText title,desc;
+    String titlesend,descsend;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.events);
-
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setSelectedItemId(R.id.action_Events);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-
-                    case R.id.action_Posts:
-                        startActivity(new Intent(getApplicationContext(), Home.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-
-                    case R.id.action_Events:
-                        startActivity(new Intent(getApplicationContext(), EventPost.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                }
-
-                return false;
-            }
-        });
-
-//......................HOOKS.......................
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        toggle = new ActionBarDrawerToggle(this,
-                drawerLayout,
-                toolbar,
-                R.string.open,
-                R.string.close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.setDrawerIndicatorEnabled(true);
-        toggle.syncState();
+        setContentView(R.layout.event_new);
 
 
+    }
 
+    public void AddEvent(View view) {
+        titlesend=title.getText().toString();
+        descsend=desc.getText().toString();
+        if(TextUtils.isEmpty(titlesend) || TextUtils.isEmpty(descsend)){
+            return;
+        }
+        Add(titlesend,descsend);
 
-/*
-        Intent intent = getIntent();
-        fullname= intent.getStringExtra("full_name_from_login");
-        fullName  = findViewById(R.id.name_poster);
-        fullName.setText(fullname);
-*/
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+    }
 
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    private void Add(String titlesend, String descsend)
+    {
 
-                // Handle navigation view item clicks here.
-
-                switch (item.getItemId()) {
-
-                    case R.id.nav_pro:
-                        Intent intent = new Intent(EventPost.this, Dashboard.class);
-                        startActivity(intent);
-                        intent.addFlags((Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                        break;
-                    case R.id.nav_post:
-                        Intent i = new Intent(EventPost.this, NewPost.class);
-                        startActivity(i);
-
-                        break;
-                    case R.id.nav_event:
-
-                        break;
-                    case R.id.nav_jobs:
-
-                        break;
-                    case R.id.nav_logout:
-                        FirebaseAuth.getInstance().signOut();//logout
-                        startActivity(new Intent(getApplicationContext(), Login.class));
-                        finish();
-
-
-                        break;
-
-
-                }
-
-                return true;
-            }
-
-        });
-
-
+        String id=mDatabase.push().getKey();
+        Listdata listdata = new Listdata(id,titlesend, descsend);
+        mDatabase.child("Events").child(id).setValue(listdata).
+                addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(EventPost.this, "Event Added", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),EventHomeScreen.class));
+                    }
+                });
     }
 }
--- app/src/main/java/com/pro/ssetalumni/Home.java
+++ app/src/main/java/com/pro/ssetalumni/Home.java
@@ -8,8 +8,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -20,6 +22,7 @@ import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -28,6 +31,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,7 +72,7 @@ public class Home extends AppCompatActivity {
 
                         return true;
                     case R.id.action_Events:
-                        startActivity(new Intent(getApplicationContext(),EventPost.class));
+                        startActivity(new Intent(getApplicationContext(),EventHomeScreen.class));
                         overridePendingTransition(0,0);
                         return true;
                 }
@@ -121,6 +125,9 @@ public class Home extends AppCompatActivity {
                         break;
                     case R.id.nav_event:
 
+                        Intent j = new Intent(Home.this, EventPost.class);
+                        startActivity(j);
+                        finish();
                         break;
                     case R.id.nav_jobs:
 
@@ -176,7 +183,7 @@ public class Home extends AppCompatActivity {
             }
         });
 
-    }
 
+    }
 
 }
